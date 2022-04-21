53e365c11ef18acf5d8dc94c927d746f1c5f954c
ayham-kattan <ayhamkattan1@gmail.com>
2022-04-21 17:32:42 +0300

    sec

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
+import App from "./App";
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+  <StrictMode>
     <BrowserRouter>
     <App />
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </StrictMode>
 );
-
